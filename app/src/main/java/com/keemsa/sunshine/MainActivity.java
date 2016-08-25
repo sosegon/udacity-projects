74659716eb82a303fe3e5a24a929257eb6e0a4c8
Sebastian Velasquez <anse23@hotmail.com>
2016-08-25 13:05:25 -0500

    Initial commit

---
+++ app/src/main/java/com/keemsa/sunshine/MainActivity.java
@@ -0,0 +1,15 @@
+package com.keemsa.sunshine;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
