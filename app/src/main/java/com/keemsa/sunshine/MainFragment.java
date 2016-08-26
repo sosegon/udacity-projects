657ff2dfa92856b0c325c1fe461ccf54c5131af1
Sebastian Velasquez <anse23@hotmail.com>
2016-08-25 20:59:39 -0500

    ui: Added fake data and adapter
    Added fake data to see how list items are displayed in list view.
    ArrayAdapter created to bind fake data with AdapterView.

--- app/src/main/java/com/keemsa/sunshine/MainFragment.java
+++ app/src/main/java/com/keemsa/sunshine/MainFragment.java
@@ -1,12 +1,16 @@
 package com.keemsa.sunshine;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainFragment extends Fragment {
 
@@ -17,6 +21,20 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        String[] data = {
+                "Mon 6/23 - Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"
+        };
+
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
+        ListView lv = (ListView) getActivity().findViewById(R.id.listview_forecast);
+        lv.setAdapter(adapter);
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
