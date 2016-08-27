bac969d59df705dab17b7c131f14599ad67bb99d
Sebastian Velasquez <anse23@hotmail.com>
2016-08-26 21:53:21 -0500

    feat: Added menu with refresh button
    Created forecastfragment.xml to define a menu for the application.
    The menu is inflated in ForecastFragment.
    Refresh option has no logic yet.

--- app/src/main/java/com/keemsa/sunshine/ForecastFragment.java
+++ app/src/main/java/com/keemsa/sunshine/ForecastFragment.java
@@ -3,6 +3,9 @@ package com.keemsa.sunshine;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -15,7 +18,7 @@ import java.util.List;
 public class ForecastFragment extends Fragment {
 
     public ForecastFragment() {
-        // Required empty public constructor
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -43,4 +46,19 @@ public class ForecastFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Inflate menu
+        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.mit_settings:
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
