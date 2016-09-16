805bbc5b4e5165f502d92cc399193247900d3d40
Sebastian <anse23@hotmail.com>
2016-09-16 14:10:47 -0500

    refactor: Modified forecast and detail fragments to use Loaders
    Fragments were modified to use Loaders to get data to fill the UI.
    Each fragment implements LoaderManager.LoaderCallbacks<Cursor>. onStart function
    was removed from ForecastFragment to avoid fetching data from openweathermap.org
    every time the device is rotated. onLocationChanged fetches data from the service
    when the location has changed under settings, this is done calling that function
    within onResume.
    
    DetailFragment defines FORECAST_COLUMNS to be passed as a projection when
    querying the database. This allows to use defined values for the columns of
    a query without using Cursor.getColumnIndex. The ShareActionProvider object
    is set with a share intent when the loader has finished loading data (onLoadFinished)
    and when creating the menu (onCreateOptionsMenu).

--- app/src/main/java/com/keemsa/sunshine/DetailFragment.java
+++ app/src/main/java/com/keemsa/sunshine/DetailFragment.java
@@ -2,8 +2,15 @@ package com.keemsa.sunshine;
 
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -12,12 +19,34 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.keemsa.sunshine.data.WeatherContract;
+
 /**
  * A simple {@link Fragment} subclass.
  */
-public class DetailFragment extends Fragment {
+public class DetailFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>{
 
     private TextView txt_forecast;
+    private String sForecast;
+    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+
+    private ShareActionProvider mShareActionProvider;
+
+    private static final int WEATHER_WITH_LOCATION_AND_DATE_LOADER = 1;
+    private static final String[] FORECAST_COLUMNS = {
+            WeatherContract.WeatherEntry.TABLE_NAME + "." + WeatherContract.WeatherEntry._ID,
+            WeatherContract.WeatherEntry.COLUMN_DATE,
+            WeatherContract.WeatherEntry.COLUMN_SHORT_DESC,
+            WeatherContract.WeatherEntry.COLUMN_MIN_TEMP,
+            WeatherContract.WeatherEntry.COLUMN_MAX_TEMP
+    };
+
+    private static final int COL_WEATHER_ID = 0;
+    private static final int COL_DATE = 1;
+    private static final int COL_SHORT_DESC = 2;
+    private static final int COL_MIN_TEMP = 3;
+    private static final int COL_MAX_TEMP = 4;
+
     public DetailFragment() {
         setHasOptionsMenu(true);
     }
@@ -30,32 +59,87 @@ public class DetailFragment extends Fragment {
 
         txt_forecast = (TextView) view.findViewById(R.id.txt_forecast);
 
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        getLoaderManager().initLoader(WEATHER_WITH_LOCATION_AND_DATE_LOADER, null, this);
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
         Intent intent = getActivity().getIntent();
 
-        if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT)){
-            txt_forecast.setText(intent.getStringExtra(Intent.EXTRA_TEXT));
+        if(intent == null){
+            return null;
         }
+        return new CursorLoader(
+                getContext(),
+                intent.getData(),
+                FORECAST_COLUMNS,
+                null,
+                null,
+                null
+        );
+    }
 
-        return view;
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        if(data.moveToFirst()){
+            String sDate = Utility.formatDate(data.getLong(COL_DATE));
+            String sDesc = data.getString(COL_SHORT_DESC);
+
+            boolean isMetric = Utility.isMetric(getActivity());
+
+            String sHigh = Utility.formatTemperature(data.getDouble(COL_MAX_TEMP), isMetric);
+            String sLow = Utility.formatTemperature(data.getDouble(COL_MIN_TEMP), isMetric);
+
+            sForecast = String.format("%s - %s - %s/%s", sDate, sDesc, sLow, sHigh);
+            txt_forecast.setText(sForecast);
+
+            if(mShareActionProvider != null){
+                mShareActionProvider.setShareIntent(createShareForecastIntent());
+            }
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         // Inflate menu
         inflater.inflate(R.menu.detailfragment, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.mit_share);
+
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+
+        if(sForecast != null && mShareActionProvider != null){
+            mShareActionProvider.setShareIntent(createShareForecastIntent());
+        }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.mit_share:
-                Intent intent1 = new Intent(Intent.ACTION_SEND);
-                intent1.putExtra(Intent.EXTRA_TEXT, txt_forecast.getText().toString() + " #SunshineApp");
-                intent1.setType("text/plain");
-                startActivity(intent1);
+                startActivity(createShareForecastIntent());
                 break;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private Intent createShareForecastIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, sForecast + FORECAST_SHARE_HASHTAG);
+        return shareIntent;
     }
 }
--- app/src/main/java/com/keemsa/sunshine/ForecastFragment.java
+++ app/src/main/java/com/keemsa/sunshine/ForecastFragment.java
@@ -2,10 +2,15 @@ package com.keemsa.sunshine;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -14,41 +19,93 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.util.ArrayList;
+import com.keemsa.sunshine.data.WeatherContract;
 
-public class ForecastFragment extends Fragment {
+public class ForecastFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor>{
 
     private final String LOG_TAG = ForecastFragment.class.getSimpleName();
-    private ArrayAdapter<String> adapter;
+    private String mLocation;
+    private ForecastAdapter adapter;
+
+    private static final int WEATHER_LOADER_ID = 1;
 
     public ForecastFragment() {
         setHasOptionsMenu(true);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_forecast, container, false);
 
         // The layout has to be inflated before searching for elements on it
-        adapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, new ArrayList<String>());
-        ListView lv = (ListView) view.findViewById(R.id.listview_forecast);
+        adapter = new ForecastAdapter(getContext(), null, 0);
+        final ListView lv = (ListView) view.findViewById(R.id.listview_forecast);
         lv.setAdapter(adapter);
+
+        // Init loader
+        getLoaderManager().initLoader(WEATHER_LOADER_ID, null, this);
+
+        // Add listener to detailed view
+        final String locationSetting = Utility.getPreferredLocation(getContext());
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Cursor cursor = (Cursor) lv.getItemAtPosition(i);
                 Intent intent = new Intent(getContext(), DetailActivity.class);
-                intent.putExtra(Intent.EXTRA_TEXT, adapter.getItem(i));
+                intent.setData(
+                        WeatherContract.WeatherEntry.buildWeatherLocationWithDate(
+                                locationSetting,
+                                cursor.getLong(ForecastAdapter.COL_WEATHER_DATE)
+                        )
+                );
                 startActivity(intent);
             }
         });
 
+        // set mLocation
+        mLocation = Utility.getPreferredLocation(getContext());
+
         return view;
+    }
+
+    public void onLocationChanged(){
+        updateWeather();
+        getLoaderManager().restartLoader(WEATHER_LOADER_ID, null, this);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        getLoaderManager().initLoader(WEATHER_LOADER_ID, null, this);
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String locationSetting = Utility.getPreferredLocation(getContext());
+        String sortOrder = WeatherContract.WeatherEntry.COLUMN_DATE + " ASC";
+        return new CursorLoader(
+                getContext(),
+                WeatherContract.WeatherEntry.buildWeatherLocationWithStartDate(locationSetting, System.currentTimeMillis()),
+                ForecastAdapter.FORECAST_COLUMNS,
+                null,
+                null,
+                sortOrder
+        );
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        adapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        adapter.swapCursor(null);
     }
 
     @Override
@@ -58,9 +115,14 @@ public class ForecastFragment extends Fragment {
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
-        updateWeather();
+    public void onResume() {
+        super.onResume();
+
+        String location = Utility.getPreferredLocation(getContext());
+        if(location != null && !mLocation.equals(location)){
+            onLocationChanged();
+            mLocation = location;
+        }
     }
 
     @Override
@@ -100,7 +162,7 @@ public class ForecastFragment extends Fragment {
     }
 
     private void updateWeather() {
-        FetchWeatherTask task = new FetchWeatherTask(this.getContext(), adapter);
+        FetchWeatherTask task = new FetchWeatherTask(this.getContext());
         SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getContext());
         task.execute(pref.getString(getString(R.string.prf_location_key), getString(R.string.prf_location_default)));
     }
