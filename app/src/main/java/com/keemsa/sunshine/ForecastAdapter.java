8763759db5c9c25dc132b6894130d9983f45e11f
Sebastian <anse23@hotmail.com>
2016-09-18 14:32:23 -0500

    ui: Modified forecast list item
    The xml layout was modified to make the list item more appealing.
    Style has been applied to order the elements in the item.
    It's using linear layout to make easier to sort the items.
    Logic bindView in the forecast adapter has been commented out to
    avoid errors since the layout is different now.

--- app/src/main/java/com/keemsa/sunshine/ForecastAdapter.java
+++ app/src/main/java/com/keemsa/sunshine/ForecastAdapter.java
@@ -6,7 +6,6 @@ import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.keemsa.sunshine.data.WeatherContract;
 
@@ -90,7 +89,7 @@ public class ForecastAdapter extends CursorAdapter {
         // our view is pretty simple here --- just a text view
         // we'll keep the UI functional with a simple (and slow!) binding.
 
-        TextView tv = (TextView) view.findViewById(R.id.list_item_forecast_textview);
-        tv.setText(convertCursorRowToUXFormat(cursor));
+        // TextView tv = (TextView) view.findViewById(R.id.list_item_forecast_textview);
+        // tv.setText(convertCursorRowToUXFormat(cursor));
     }
 }
