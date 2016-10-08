4f585ad3d8c9b11679d5adb5adf945c03b328254
Sebastian <anse23@hotmail.com>
2016-10-07 19:24:28 -0500

    feat: Modified MovieAdapter to be used with loaders
    MovieAdapter has been converted to a CursorAdapter to be used with loaders
    to take advantage of the content provider created to communicate with the
    database.

--- app/src/main/java/com/keemsa/popularmovies/MovieAdapter.java
+++ app/src/main/java/com/keemsa/popularmovies/MovieAdapter.java
@@ -1,42 +1,49 @@
 package com.keemsa.popularmovies;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.CursorAdapter;
 import android.widget.ImageView;
 
-import com.keemsa.popularmovies.model.Movie;
+import com.keemsa.popularmovies.data.MovieColumns;
 import com.squareup.picasso.Picasso;
-
-import java.util.List;
 
 /**
  * Created by sebastian on 31/08/16.
  */
-public class MovieAdapter extends ArrayAdapter<Movie> {
+public class MovieAdapter extends CursorAdapter {
+
+    public static class ViewHolder {
+        public final ImageView posterView;
+
+        public ViewHolder(View view){
+            posterView = (ImageView) view.findViewById(R.id.imv_movie_poster);
+        }
+    }
 
     private String LOG_TAG = MovieAdapter.class.getSimpleName();
+    private ViewHolder holder;
 
-    public MovieAdapter(Context context, int resource, List<Movie> objects) {
-        super(context, resource, objects);
+    public MovieAdapter(Context context, Cursor c, int flag) {
+        super(context, c, flag);
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public void bindView(View view, Context context, Cursor cursor) {
+        int posterUrlIndex = cursor.getColumnIndex(MovieColumns.POSTER_URL);
+        String posterUrl = cursor.getString(posterUrlIndex);
+        String fullPosterUrl = Uri.parse(context.getString(R.string.base_img_url)).buildUpon().appendPath(posterUrl).build().toString();
+        Picasso.with(context).load(fullPosterUrl).into(holder.posterView);
+    }
 
-        Movie movie = getItem(position);
-
-        if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_movie, parent, false);
-        }
-
-        ImageView imv_movie_poster = (ImageView) convertView.findViewById(R.id.imv_movie_poster);
-
-        String posterFullUrl = getContext().getString(R.string.base_img_url) + movie.getPosterUrl();
-        Picasso.with(getContext()).load(posterFullUrl).into(imv_movie_poster);
-
-        return convertView;
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_movie, viewGroup, false);
+        holder = new ViewHolder(view);
+        return view;
     }
 }
