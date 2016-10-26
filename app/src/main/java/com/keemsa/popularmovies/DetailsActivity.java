2378be04129f277a6553a3cb409554959865301a
Sebastian <anse23@hotmail.com>
2016-10-25 21:09:19 -0500

    feat: Added toolbar to DetailsActivity
    Added the toolbar to allow the up navigation. Also, modified the style of the title
    of the movie so it does not look bigger than the toolbar.

--- app/src/main/java/com/keemsa/popularmovies/DetailsActivity.java
+++ app/src/main/java/com/keemsa/popularmovies/DetailsActivity.java
@@ -1,9 +1,9 @@
 package com.keemsa.popularmovies;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 
-public class DetailsActivity extends FragmentActivity {
+public class DetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +13,7 @@ public class DetailsActivity extends FragmentActivity {
         /* TODO: I don't add the fragment programatically
            I simply added the fragment in the xml file as oppose
            to what was done in Sunshine app.
-           Is there any disadvantage related to this approach
+           Is there any disadvantage related to this approach?
          */
     }
 }
