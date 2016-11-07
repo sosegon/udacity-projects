c4f7e5bfbfeb7c74d83f57ca69b3a2a7f1e60314
Sebastian <anse23@hotmail.com>
2016-11-07 09:56:43 -0500

    feat: Implement GCM to receive notifications
    I followed the guidelines provided in https://classroom.udacity.com/courses/ud855/lessons/3995738628/concepts/43227496620923#
    but it seems that the implementation is not well done. I will have to check the FCM
    to update this feature.

--- app/src/main/java/com/keemsa/popularmovies/CatalogActivity.java
+++ app/src/main/java/com/keemsa/popularmovies/CatalogActivity.java
@@ -1,8 +1,10 @@
 package com.keemsa.popularmovies;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -14,6 +16,7 @@ import com.google.android.gms.common.GoogleApiAvailability;
 import com.keemsa.popularmovies.fragment.CatalogFragment;
 import com.keemsa.popularmovies.fragment.DetailsFragment;
 import com.keemsa.popularmovies.fragment.SearchFragment;
+import com.keemsa.popularmovies.gcm.RegistrationIntentService;
 import com.keemsa.popularmovies.sync.MoviesSyncAdapter;
 
 public class CatalogActivity extends AppCompatActivity implements MovieSelectedInterface {
@@ -23,6 +26,7 @@ public class CatalogActivity extends AppCompatActivity implements MovieSelectedI
     private final String CATALOG_FRAGMENT_TAG = "CFTAG";
     private final String SEARCH_FRAGMENT_TAG = "SFTAG";
     private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
     private String mQueryBy;
     private boolean mTwoPane = false;
     private FragmentTabHost mtabHost;
@@ -76,9 +80,15 @@ public class CatalogActivity extends AppCompatActivity implements MovieSelectedI
         }
 
         if (!checkPlayServices()) {
-            // This is where we could either prompt a user that they should install
-            // the latest version of Google Play Services, or add an error snackbar
-            // that some features won't be available.
+            // Because this is the initial creation of the app, we'll want to be certain we have
+            // a token. If we do not, then we will start the IntentService that will register this
+            // application with GCM.
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+            boolean sentToken = sharedPreferences.getBoolean(SENT_TOKEN_TO_SERVER, false);
+            if (!sentToken) {
+                Intent intent = new Intent(this, RegistrationIntentService.class);
+                startService(intent);
+            }
         }
     }
 
