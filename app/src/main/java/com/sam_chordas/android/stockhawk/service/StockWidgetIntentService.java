0ed1cd3f98abfefaa7e4b1d2148a96c8958d7284
Sebastian <anse23@hotmail.com>
2016-12-09 20:15:15 -0500

    feat: Added widget for +3 columns
    This new widget displays the percentage change of the stock. To accomplish this,
    simply verify the size of the widget and compare to the default size to set
    the corresponding layout resource.

--- app/src/main/java/com/sam_chordas/android/stockhawk/service/StockWidgetIntentService.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/service/StockWidgetIntentService.java
@@ -1,13 +1,19 @@
 package com.sam_chordas.android.stockhawk.service;
 
+import android.annotation.TargetApi;
 import android.app.IntentService;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.TypedValue;
 import android.widget.RemoteViews;
+import android.widget.TextView;
 
 import com.google.android.gms.gcm.TaskParams;
 import com.sam_chordas.android.stockhawk.R;
@@ -52,6 +58,7 @@ public class StockWidgetIntentService extends IntentService {
 
     String stockName = c.getString(Projections.STOCK_SYMBOL);
     String stockPrice = c.getString(Projections.STOCK_BIDPRICE);
+    String changePercentage = c.getString(Projections.STOCK_PERCENT_CHANGE);
     int isUp = c.getInt(Projections.STOCK_ISUP);
 
     c.close();
@@ -60,10 +67,36 @@ public class StockWidgetIntentService extends IntentService {
     int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(this, StockWidgetProvider.class));
 
     for(int appWidgetId : appWidgetIds){
-      RemoteViews views = new RemoteViews(getPackageName(), R.layout.widget_stock);
+      int widgetWidth = getWidgetWidth(appWidgetManager, appWidgetId);
+      int defaultWidth = getResources().getDimensionPixelSize(R.dimen.widget_stock_default_width);
+      int layoutId;
+
+      if(widgetWidth <= defaultWidth){
+        layoutId = R.layout.widget_stock;
+      } else {
+        layoutId = R.layout.widget_stock_large;
+      }
+
+      RemoteViews views = new RemoteViews(getPackageName(), layoutId);
 
       views.setTextViewText(R.id.txt_w_name, stockName);
       views.setTextViewText(R.id.txt_w_price, stockPrice);
+      views.setTextViewText(R.id.txt_w_change, changePercentage);
+
+      int sdk = Build.VERSION.SDK_INT;
+      if (isUp == 1){
+        if (sdk < Build.VERSION_CODES.JELLY_BEAN){
+          views.setInt(R.id.txt_w_change, "setBackgroundDrawable", R.drawable.percent_change_pill_green);
+        }else {
+          views.setInt(R.id.txt_w_change, "setBackgroundResource", R.drawable.percent_change_pill_green);
+        }
+      } else{
+        if (sdk < Build.VERSION_CODES.JELLY_BEAN) {
+          views.setInt(R.id.txt_w_change, "setBackgroundDrawable", R.drawable.percent_change_pill_red);
+        } else{
+          views.setInt(R.id.txt_w_change, "setBackgroundResource", R.drawable.percent_change_pill_red);
+        }
+      }
 
       Intent launchIntent = new Intent(this, MyStocksActivity.class);
       PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, launchIntent, 0);
@@ -71,5 +104,31 @@ public class StockWidgetIntentService extends IntentService {
 
       appWidgetManager.updateAppWidget(appWidgetId, views);
     }
+  }
+
+  // From https://github.com/udacity/Advanced_Android_Development/blob/2e1698d0f55639af434dc8d96bd7e11d734537b9/app/src/main/java/com/example/android/sunshine/app/widget/TodayWidgetIntentService.java#L124
+  private int getWidgetWidth(AppWidgetManager appWidgetManager, int appWidgetId){
+    // Previous to Jelly Bean, widgets were always their default size
+    if(Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN){
+      return getResources().getDimensionPixelSize(R.dimen.widget_stock_default_width);
+    }
+
+    return getWidgetWidthFromOptions(appWidgetManager, appWidgetId);
+  }
+
+  // From https://github.com/udacity/Advanced_Android_Development/blob/2e1698d0f55639af434dc8d96bd7e11d734537b9/app/src/main/java/com/example/android/sunshine/app/widget/TodayWidgetIntentService.java#L135
+  @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+  private int getWidgetWidthFromOptions(AppWidgetManager appWidgetManager, int appWidgetId){
+    Bundle options = appWidgetManager.getAppWidgetOptions(appWidgetId);
+
+    if(options.containsKey(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH)){
+      int minWidthDp = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH);
+
+      DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+
+      return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, minWidthDp, displayMetrics);
+    }
+
+    return  getResources().getDimensionPixelSize(R.dimen.widget_stock_default_width);
   }
 }
