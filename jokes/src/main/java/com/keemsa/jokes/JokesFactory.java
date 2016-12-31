f24643c00cea9a7d6023e7f3ea7c60da3a10694c
Sebastian <anse23@hotmail.com>
2016-12-31 17:39:27 -0500

    refactor: Added actual jokes to the JokesFactory
    Android jokes just for fun.

--- jokes/src/main/java/com/keemsa/jokes/JokesFactory.java
+++ jokes/src/main/java/com/keemsa/jokes/JokesFactory.java
@@ -9,14 +9,15 @@ public class JokesFactory {
   private static JokesFactory factory;
 
   private String[] jokes = {
-          "joke1",
-          "joke2",
-          "joke3",
-          "joke4",
-          "joke5",
-          "joke6",
-          "joke7",
-          "joke8",
+          "An Android app walks into a bar. Bartender asks, \"Can I get you a drink?\" The app looks disappointed and says, \"That wasn't my intent.\"",
+          "A Gingerbread phone walks into a bar and starts screaming obscenities. An irate patron, having had enough of Gingerbread's nonsense, throws a direct punch... but somehow misses. Gingerbread erupts with laugher and says, \"Lololol! You have no idea where my touch area is!\"",
+          "A fragment walks into a bar and says, \"I'm lost. Can I please speak to the FragmentManager?\"",
+          "A pregnant fragment walks into a bar. The bartender says, \"Whoa! Whoa! We don't support nested fragments here!\"",
+          "An iPhone enters the Android bar. The customers take him out and beat him up. Just before passing out, he says \"I swear, I thought this was an iPhone bar\".\n" +
+                  "The angry Androids reply: \"sorry, you can't use that excuse anymore!\"",
+          "A fragment walks into a bar, and the bartender asks for an ID. Fragment says, \"I don't have an ID.\" So the bartender says, \"Okay, I'll make a NullPointerException.\"",
+          "A ChaCha walks into a bar. The bartender asks, \"What will it be?\". \"I'm sorry\", says the ChaCha, \"I'm already full\".",
+          "A WebView walks into a bar. The bartender says, \"Sorry buddy, we only allow natives here.\" Disappointed, the WebView walks out of the bar.. and then gets ran over by a Mustang.",
   };
 
   private JokesFactory(){
