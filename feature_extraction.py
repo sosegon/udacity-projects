fix: Replaced original last layer of Alexnet, with new layer for traffic signs classification.

feature_extraction.py
@@ -17,7 +17,13 @@ fc7 = AlexNet(resized, feature_extract=True)
 # TODO: Define a new fully connected layer followed by a softmax activation to classify
 # the traffic signs. Assign the result of the softmax activation to `probs` below.
 shape = (fc7.get_shape().as_list()[-1], nb_classes)  # use this shape for the weight matrix
-probs = ...
+print(shape)
+weights = tf.Variable(tf.truncated_normal(shape))
+biases = tf.Variable(tf.zeros(nb_classes))
+
+fc8 = tf.add(tf.matmul(fc7, weights), biases)
+
+probs = tf.nn.softmax(fc8)
 
 init = tf.initialize_all_variables()
 sess = tf.Session()