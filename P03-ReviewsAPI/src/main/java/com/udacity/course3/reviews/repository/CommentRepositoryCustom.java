ca173ad7f8c4c3b8cb57222a4fe3d3b8deb07174
Sebastian <anse23@hotmail.com>
2019-08-09 15:50:03 -0500

    refactor: Set queries with @Query
    The implementation of custon methods for the
    repository are avoided by using native SQL
    queries.

--- P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/repository/CommentRepositoryCustom.java
+++ P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/repository/CommentRepositoryCustom.java
@@ -1,9 +1,18 @@
 package com.udacity.course3.reviews.repository;
 
 import com.udacity.course3.reviews.model.Comment;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
+@NoRepositoryBean
 public interface CommentRepositoryCustom {
-    List<Comment> findCommentsByReviewId(int reviewId);
+
+    @Query(
+            value = "SELECT * FROM comments c " +
+                    "WHERE c.review_id = :review_id",
+            nativeQuery = true)
+    List<Comment> findCommentsByReviewId(@Param("review_id") int reviewId);
 }
--- P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/repository/ReviewRepositoryCustom.java
+++ P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/repository/ReviewRepositoryCustom.java
@@ -1,12 +1,30 @@
 package com.udacity.course3.reviews.repository;
 
 import com.udacity.course3.reviews.model.Review;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Date;
 import java.util.List;
 
+@NoRepositoryBean
 public interface ReviewRepositoryCustom {
-    List<Review> findReviewsByProductId(int productId);
-    List<Review> findReviewsByProductIdRatingAtLeast(int productId, int rating);
-    List<Review> findReviewsByProductIdSinceDate(int productId, Date date);
+    @Query(
+            value = "SELECT * FROM reviews r " +
+                    "WHERE r.product_id = :product_id",
+            nativeQuery = true)
+    List<Review> findReviewsByProductId(@Param("product_id") int productId);
+
+    @Query(
+            value = "SELECT * FROM reviews r " +
+                    "WHERE r.product_id = :product_id AND r.rating >= :rating",
+            nativeQuery = true)
+    List<Review> findReviewsByProductIdRatingAtLeast(@Param("product_id") int productId, @Param("rating") int rating);
+
+    @Query(
+            value = "SELECT * FROM reviews r " +
+                    "WHERE r.product_id = :product_id AND r.date_creation >= :date",
+            nativeQuery = true)
+    List<Review> findReviewsByProductIdSinceDate(@Param("product_id") int productId, @Param("date") Date date);
 }
