3c01f2fc1ff3ebd8a2221c7ecb486a23ac3e1172
Sebastian <anse23@hotmail.com>
2019-08-09 17:01:19 -0500

    feat: Enabled JPA repositories in application

--- P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/ReviewsApplication.java
+++ P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/ReviewsApplication.java
@@ -1,9 +1,11 @@
-package com.udacity.course3.reviews;
+package com.udacity. course3.reviews;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class ReviewsApplication {
 
 	public static void main(String[] args) {
