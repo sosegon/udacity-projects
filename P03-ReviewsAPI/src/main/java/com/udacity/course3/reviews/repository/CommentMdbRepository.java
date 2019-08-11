88c6abf63a604b5d111a6c20651b9c735e256a58
Sebastian <anse23@hotmail.com>
2019-08-10 21:03:23 -0500

    feat: Added repositories for mongodb entities

--- P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/repository/CommentMdbRepository.java
+++ P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/repository/CommentMdbRepository.java
@@ -1,4 +1,12 @@
 package com.udacity.course3.reviews.repository;
 
-public interface CommentMdbRepository {
+import com.udacity.course3.reviews.model.CommentMdb;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentMdbRepository extends MongoRepository<CommentMdb, Integer> {
+    List<CommentMdb> findByReviewId(int reviewId);
 }
--- P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/repository/ReviewMdbRepository.java
+++ P03-ReviewsAPI/src/main/java/com/udacity/course3/reviews/repository/ReviewMdbRepository.java
@@ -1,4 +1,12 @@
 package com.udacity.course3.reviews.repository;
 
-public interface ReviewMdbRepository {
+import com.udacity.course3.reviews.model.ReviewMdb;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReviewMdbRepository extends MongoRepository<ReviewMdb, Integer> {
+    List<ReviewMdb> findByProductId(int productId);
 }
