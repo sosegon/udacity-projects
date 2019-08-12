c09f9e1450edb37e342bdb01cf98e011275a8ae8
Sebastian <anse23@hotmail.com>
2019-08-12 16:09:07 -0500

    test: Added tests for mongo repository

--- P03-ReviewsAPI/src/test/java/com/udacity/course3/reviews/ReviewsMongoDbSpringIntegrationTest.java
+++ P03-ReviewsAPI/src/test/java/com/udacity/course3/reviews/ReviewsMongoDbSpringIntegrationTest.java
@@ -1,16 +1,19 @@
 package com.udacity.course3.reviews;
 
+import com.udacity.course3.reviews.model.CommentDocument;
 import com.udacity.course3.reviews.model.ReviewDocument;
 import com.udacity.course3.reviews.repository.ReviewMongoRepository;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 @RunWith(SpringRunner.class)
@@ -20,17 +23,59 @@ public class ReviewsMongoDbSpringIntegrationTest {
     @Autowired
     private ReviewMongoRepository reviewMongoRepository;
 
-    @DisplayName("given object to save"
-            + " when save object using MongoDB template"
-            + " then object is saved")
-    @Test
-    public void testFindByProductId() {
-        ReviewDocument review = new ReviewDocument("New review", new Date(), 5, 1);
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
+    @Test
+    public void testSaveReview() {
+        ReviewDocument review = new ReviewDocument("New review", new Date(), 5, 1);
+        review.setId(1);
+
+        mongoTemplate.save(review, "reviews");
+
+        Assert.assertTrue(!mongoTemplate.findAll(ReviewDocument.class, "reviews").isEmpty());
+        Assert.assertEquals(1, mongoTemplate.findById(1, ReviewDocument.class).getId());
+    }
+
+    @Test
+    public void testFindReviewById() {
+        ReviewDocument review = new ReviewDocument("New review", new Date(), 5, 1);
+        review.setId(1);
+
+        reviewMongoRepository.save(review);
+
+        Optional<ReviewDocument> opReview = reviewMongoRepository.findById(review.getId());
+
+        Assert.assertEquals(review.getId(), opReview.get().getId());
+    }
+
+    @Test
+    public void testFindReviewsByProductId() {
+        ReviewDocument review1 = new ReviewDocument("New review", new Date(), 5, 1);
+        review1.setId(0);
+        ReviewDocument review2 = new ReviewDocument("New review", new Date(), 5, 1);
+        review2.setId(1);
+
+        ReviewDocument nReview1 = reviewMongoRepository.save(review1);
+        ReviewDocument nReview2 = reviewMongoRepository.save(review2);
+
+        List<ReviewDocument> reviews = reviewMongoRepository.findReviewsByProductId(1);
+
+        Assert.assertEquals(2, reviews.size());
+    }
+
+    @Test
+    public void testFindCommentsByReviewId() {
+        ReviewDocument review = new ReviewDocument("New review", new Date(), 5, 1);
+        review.setId(1);
+        List<CommentDocument> comments = new ArrayList<CommentDocument>();
+        comments.add(new CommentDocument("new comment 1", new Date()));
+        comments.add(new CommentDocument("new comment 2", new Date()));
+        review.setComments(comments);
         ReviewDocument nReview = reviewMongoRepository.save(review);
 
         Optional<ReviewDocument> opReview = reviewMongoRepository.findById(nReview.getId());
 
-        Assert.assertEquals(nReview.getId(), opReview.get().getId());
+        Assert.assertEquals(2, opReview.get().getComments().size());
     }
 }
